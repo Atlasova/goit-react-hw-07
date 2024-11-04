881fcba7d8dbf627208b704493f7b2365087534e
Juliia Prokopenko <atlasovaa13@gmail.com>
2024-11-04 17:28:16 +0100

    fix bug ContactList/selectFilterContacts

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -1,26 +1,26 @@
 import { useSelector } from 'react-redux';
 import Contact from '../Contact/Contact';
 import css from './ContactList.module.css';
-import { selectContacts, selectNameFilter } from '../../redux/selectors';
+import { selectFilteredContacts } from '../../redux/selectors';
 
 const ContactList = () => {
-  const contacts = useSelector(selectContacts);
-  const filter = useSelector(selectNameFilter);
+  const filteredContacts = useSelector(selectFilteredContacts);
 
-  const filterData = contacts.filter(item =>
-    item.name.toLowerCase().includes(filter.toLowerCase())
-  );
   return (
     <ul className={css.contactList}>
-      {filterData.map(contact => (
-        <li key={contact.id} className={css.contactItem}>
-          <Contact
-            id={contact.id}
-            name={contact.name}
-            number={contact.number}
-          />
-        </li>
-      ))}
+      {filteredContacts.length ? (
+        filteredContacts.map(contact => (
+          <li key={contact.id} className={css.contactItem}>
+            <Contact
+              name={contact.name}
+              number={contact.number}
+              id={contact.id}
+            />
+          </li>
+        ))
+      ) : (
+        <h2>No contacts!</h2>
+      )}
     </ul>
   );
 };
